2441cfc19e5dc12e9b4111e5bd2487a611a1470a
Jakub Żemajduk <kubazemajduk1@gmail.com>
2025-05-13 12:57:36 +0200

    .

--- App/MediCare/obj/Debug/net8.0-windows/MediCare.AssemblyInfo.cs
+++ App/MediCare/obj/Debug/net8.0-windows/MediCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18aa543a2d543db9e979bf0ee5ad7f803e37b0a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21dbac9b5a048aae2ccd1730b8e14859705ff391")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
