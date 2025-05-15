8a8e5dca143ab2a64d46cffd5ec3e9bce9260d62
Jakub Żemajduk <kubazemajduk1@gmail.com>
2025-05-15 17:04:53 +0200

    Refactor user model

--- App/MediCare/Data/Models/User.cs
+++ App/MediCare/Data/Models/User.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MediCare.Data.Models
 {
@@ -13,18 +6,15 @@ namespace MediCare.Data.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Adres e-mail jest wymagany.")]
-        [EmailAddress(ErrorMessage = "Nieprawidłowy adres e-mail.")]
-        [MaxLength(100, ErrorMessage = "Adres e-mail może mieć maksymalnie 100 znaków.")]
-        public string AddressEmail { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Hasło jest wymagane.")]
-        [StringLength(100, MinimumLength = 8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$",
-            ErrorMessage = "Hasło musi zawierać co najmniej jedną małą literę, jedną dużą literę oraz jedną cyfrę.")]
-        public string Password { get; set; }
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Rola użytkownika jest wymagana.")]
+        [Required]
         public UserRole Role { get; set; }
     }
 }
