f5a801926d7ebcebab0114a8bcd85f3b60d902aa
Jakub Żemajduk <kubazemajduk1@gmail.com>
2025-05-15 17:38:31 +0200

    Add user registration logic

--- App/MediCare/ViewModels/Authentication/RegisterWindow.xaml.cs
+++ App/MediCare/ViewModels/Authentication/RegisterWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediCare.Data.Controllers;
+using MediCare.Data.DTOs;
+using MediCare.Data.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,10 +25,27 @@ namespace MediCare.Views.Authentication
         {
             InitializeComponent();
         }
-        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        private async Task RegisterButton_ClickAsync(object sender, RoutedEventArgs e)
         {
-            // Logika obsługi rejestracji
-            MessageBox.Show("Rejestracja zakończona sukcesem!", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+            var registerService = App.ServiceProvider.GetRequiredService<RegisterService>();
+            var registerController = new RegisterController(registerService);
+
+            var dto = new RegisterUserDto
+            {
+                Email = EmailTextBox.Text,
+                Password = PasswordBox.Password
+            };
+
+            var (success, message) = await registerController.RegisterAsync(dto);
+
+            if (success)
+            {
+                MessageBox.Show("Rejestracja zakończona sukcesem.", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show(message, "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
     }
