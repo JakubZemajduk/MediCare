dd423389cb76adcd66bf7664904e8ab6f4911c37
Jakub Żemajduk <kubazemajduk1@gmail.com>
2025-05-20 12:19:31 +0200

    Dodano prosty widok w celu sprawdzenia rejestracji

--- App/MediCare/MainWindow.xaml.cs
+++ App/MediCare/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MediCare
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var registerWindow = new Views.Authentication.RegisterWindow();
+            registerWindow.ShowDialog();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new Views.Authentication.LoginWindow();
+            loginWindow.ShowDialog();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
